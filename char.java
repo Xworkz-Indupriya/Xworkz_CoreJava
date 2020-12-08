d2930e1ed54e29eee4bc4d13c2cde2f932b3fa18
Xworkz-Indupriya <chennaindupriya.xworkz@gmail.com>
2020-12-09 00:39:00 +0530

    Adding examples of datatypes

---
+++ char.java
@@ -0,0 +1,12 @@
+class IPLTeam
+{
+   public static void main(String a[])
+   {
+     char x='C';
+     char y='S';
+     char z='K';
+     System.out.println(x);
+     System.out.println(y);
+     System.out.println(z);
+   }
+}
