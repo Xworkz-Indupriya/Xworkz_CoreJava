4df2c4dff465decbc2e350dc7ba2d509cb473533
Xworkz-Indupriya <chennaindupriya.xworkz@gmail.com>
2020-12-09 22:05:13 +0530

    Adding examples of inheritance files

---
+++ forest/src/com/indu/induapp/forest/NalaMalaForest.java
@@ -0,0 +1,13 @@
+package com.indu.induapp.forest;
+
+public class NalaMalaForest extends Forest {
+
+	public NalaMalaForest(String address, int acres) {
+		super(address, acres);
+		
+	}
+
+	public NalaMalaForest() {
+		
+	}
+}
