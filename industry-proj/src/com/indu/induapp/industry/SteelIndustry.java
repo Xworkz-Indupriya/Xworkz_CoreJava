6ed83733065bcd0225b63858c545092f92bb52f3
Xworkz-Indupriya <chennaindupriya.xworkz@gmail.com>
2020-12-14 12:24:04 +0530

    committing industry files

---
+++ industry-proj/src/com/indu/induapp/industry/SteelIndustry.java
@@ -0,0 +1,17 @@
+package com.indu.induapp.industry;
+//derivedclass or subclass or childclass 
+public class SteelIndustry extends Industry {
+
+	public SteelIndustry(String type, int noOfWorkers, String area) {
+		super(type, noOfWorkers, area);
+	}
+
+	public SteelIndustry() {
+		
+	}
+	
+	}
+
+	
+
+
