d5d9a498493199801ccbca93757a470adb8170c7
Xworkz-Indupriya <chennaindupriya.xworkz@gmail.com>
2020-12-15 20:50:07 +0530

    Adding inheritance examples files

---
+++ institution/src/com/indu/induapp/institution/BMSInstitute.java
@@ -0,0 +1,8 @@
+package com.indu.induapp.institution;
+
+public class BMSInstitute extends Institution{
+	
+	public BMSInstitute( String address, String courses) {
+		super( address, courses);
+	}
+}
