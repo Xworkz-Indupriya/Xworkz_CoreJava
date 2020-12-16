e30a7b006240c3c27e0e9d6c7355e6249c5e6172
Xworkz-Indupriya <73948962+Xworkz-Indupriya@users.noreply.github.com>
2020-12-16 11:01:51 +0530

    Delete BMSInstitute.java

--- institution/src/com/indu/induapp/institution/BMSInstitute.java
+++
@@ -1,8 +0,0 @@
-package com.indu.induapp.institution;
-
-public class BMSInstitute extends Institution{
-	
-	public BMSInstitute( String address, String courses) {
-		super( address, courses);
-	}
-}
